d570490381f70d377c2c31262b4084f603cc133c
Raquel Costa <105187443+Raquelsc05@users.noreply.github.com>
2022-09-19 20:03:03 -0300

    20nov data

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import Countdown from './countdown.js'
 
-const tempoParaOWWB = new Countdown('21 November 2022 07:00:00 GMT-0300')
+const tempoParaOWWB = new Countdown('20 November 2022 07:00:00 GMT-0300')
 const tempos = document.querySelectorAll('[data-time]')
 
 function mostrarTempo() {
